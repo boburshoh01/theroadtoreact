1beaa5dd259ab234a3a519f0c577c5240aaed8a4
boburshoh01 <temirovbobur8@gmail.com>
2022-02-17 00:54:02 +0500

    search and list components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+import List from "./components/List";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
+      <Search />
       <hr />
-      <ul>
-        {list.map((item) => (
-          <li>{item.title}</li>
-        ))}
-      </ul>
+      <List />
     </div>
   );
 }
