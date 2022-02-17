49cfe0e36dfd08c33a3f5e6a2f814c57ce66e7cc
boburshoh01 <temirovbobur8@gmail.com>
2022-02-17 12:20:01 +0500

    handleChange

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 export default function Search() {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
+      <input type="text" id="search" onChange={handleChange} />
     </div>
   );
 }
